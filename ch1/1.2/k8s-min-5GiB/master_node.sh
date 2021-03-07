8b38c3a42869263be54952e6dac50e2dcb9fffbb
sysnet4admin <pagaia@hotmail.com>
2021-03-07 16:02:39 +0900

    upt calico ver

--- ch1/1.2/k8s-min-5GiB/master_node.sh
+++ ch1/1.2/k8s-min-5GiB/master_node.sh
@@ -13,7 +13,7 @@ chown $(id -u):$(id -g) $HOME/.kube/config
 raw_git="raw.githubusercontent.com/sysnet4admin/IaC/master/manifests" 
 
 # config for kubernetes's network 
-kubectl apply -f https://$raw_git/172.16_net_calico.yaml
+kubectl apply -f https://$raw_git/172.16_net_calico_v1.yaml
 
 # install bash-completion for kubectl 
 yum install bash-completion -y 
