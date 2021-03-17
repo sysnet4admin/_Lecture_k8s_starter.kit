14b9d5f0ac8ca9b53763c7901488164b379fb60b
sysnet4admin <pagaia@hotmail.com>
2021-03-17 20:22:25 +0900

    upt command

--- ch8/003/install_etcdctl.sh
+++ ch8/003/install_etcdctl.sh
@@ -2,4 +2,4 @@
 
 curl https://raw.githubusercontent.com/sysnet4admin/BB/main/etcdctl/v3.4.15/etcdctl -o /usr/local/bin/etcdctl
 chmod +x /usr/local/bin/etcdctl
-echo "etcd successfully installed"
+echo "etcdctl successfully installed"
