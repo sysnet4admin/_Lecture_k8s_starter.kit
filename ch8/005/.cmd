8d35c52d8fa029ebbbd6e0896e5e3c2d3d6b9f94
sysnet4admin <pagaia@hotmail.com>
2021-06-01 07:40:13 +0900

    upt polaris-lb

--- ch8/005/.cmd
+++ ch8/005/.cmd
@@ -1,6 +1,6 @@
 #Polaris
 kubectl apply -f https://github.com/fairwindsops/polaris/releases/latest/download/dashboard.yaml
-k expose deployment -n polaris polaris-dashboard --type=LoadBalancer --port=8080
+k expose deployment -n polaris polaris-dashboard --type=LoadBalancer --port=8080 --name=polaris-lb
 
 #Chaos Mesh
 curl -sSL https://mirrors.chaos-mesh.org/v1.1.2/install.sh | bash
