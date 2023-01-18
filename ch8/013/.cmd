bdea06dc371c9d905ade652b3d2fad33db2b18b6
Hoon Jo <pagaia@hotmail.com>
2023-01-18 11:01:19 +0900

    upt .cmd

---
+++ ch8/013/.cmd
@@ -0,0 +1,7 @@
+# the location of configuration file 
+# https://github.com/Eugeny/tabby/wiki/Config-file
+## On Windows, %APPDATA%/Tabby
+## On macOS: ~/Library/Application Support/tabby
+## On Linux: ~/.config/tabby
+
+cp MacOS-config.yaml ~/Library/Application Support/tabby
