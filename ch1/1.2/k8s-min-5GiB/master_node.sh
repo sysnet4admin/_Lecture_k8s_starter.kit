c02acf26951e91073ed8df78fca37cf69a76f65b
Hoon Jo <pagaia@hotmail.com>
2023-06-10 12:35:33 +0900

    chg certs from 1 to 10years

--- ch1/1.2/k8s-min-5GiB/master_node.sh
+++ ch1/1.2/k8s-min-5GiB/master_node.sh
@@ -41,3 +41,9 @@ git clone https://github.com/sysnet4admin/_Lecture_k8s_starter.kit.git $HOME/_Le
 find $HOME/_Lecture_k8s_starter.kit -regex ".*\.\(sh\)" -exec chmod 700 {} \;
 EOF
 chmod 700 /usr/local/bin/rerepo-k8s-starter.kit
+
+# extended k8s certifications all
+git clone https://github.com/yuyicai/update-kube-cert.git
+cd update-kube-cert
+chmod 755 update-kubeadm-cert.sh
+./update-kubeadm-cert.sh all
