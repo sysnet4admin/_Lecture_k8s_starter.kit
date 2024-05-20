b075e04166b7d2b1915971524a5eb1bb2cbf49df
Hoon Jo <pagaia@hotmail.com>
2024-05-20 17:22:30 +0900

    upt k8s-v1.30

--- ch1/1.2/k8s-min-5GiB/controlplane_node.sh
+++ ch1/1.2/k8s-min-5GiB/controlplane_node.sh
@@ -5,7 +5,7 @@ kubeadm init --token 123456.1234567890123456 --token-ttl 0 \
              --pod-network-cidr=172.16.0.0/16 --apiserver-advertise-address=192.168.1.10 \
              --cri-socket=unix:///run/containerd/containerd.sock
 
-# config for master node only 
+# config for control plane node only 
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
