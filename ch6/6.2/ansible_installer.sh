74a3d0d50ab4bae2fffea3c1ce084a50d234457b
Hoon Jo <pagaia@hotmail.com>
2024-05-21 12:11:52 +0900

    upt v1.30 for ch6/6.2

--- ch6/6.2/ansible_installer.sh
+++ ch6/6.2/ansible_installer.sh
@@ -10,7 +10,7 @@ apt-get install ansible sshpass -y
 ## ansible --version 
 ## ansible [core 2.16.6]
 
-# setup default ansible hosts (Ubuntu only)
+## setup default ansible hosts (Ubuntu only)
 mkdir /etc/ansible 
 cat <<EOF > /etc/ansible/hosts
 [Control_Plane]
@@ -20,23 +20,27 @@ cat <<EOF > /etc/ansible/hosts
 192.168.1.[101:103]
 EOF
 
-# ansible alias 
+## ansible alias 
 echo "alias ans=ansible"          >> ~/.bashrc
 echo "alias anp=ansible-playbook" >> ~/.bashrc
+
+## avoid [WARNING]: Updating cache and auto-installing missing dependency: python3-apt
+export ansible_python_interpreter=/usr/bin/python3
+
 
 # auto_pass to ansible_nodes 
 
 ## read hosts from file 
 readarray HOSTS < /etc/hosts
 
-### 1.known_hosts##
+## 1.known_hosts##
 if [ ! -f ~/.ssh/known_hosts ]; then
   for HOST in ${HOSTS[@]}; do
     ssh-keyscan -t ecdsa ${HOST} >> ~/.ssh/known_hosts
   done
 fi
 
-### 2.authorized_keys
+## 2.authorized_keys
 if [ ! -f ~/.ssh/id_rsa.pub ]; then
   ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N ''
     for HOST in ${HOSTS[@]}; do 
