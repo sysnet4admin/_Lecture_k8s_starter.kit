53fb71bb08915eac365d2ff64fd3684dd72fd764
Hoon Jo <pagaia@hotmail.com>
2024-05-21 11:05:28 +0900

    upt v1.30 for ch6/6.2

--- ch6/6.2/ansible_installer.sh
+++ ch6/6.2/ansible_installer.sh
@@ -2,14 +2,16 @@
 
 # ansible 
 ## install ansible 
-apt-get install python3-pip -y
-python3 -m pip install --user ansible-core==2.16.6
+#apt-get install python3-pip -y
+#python3 -m pip install --user ansible-core==2.16.6
+apt-get install ansible sshpass -y
 
-## check ansible version 
+## Tested this version  
 ## ansible --version 
 ## ansible [core 2.16.6]
 
-# setup ansible hosts 
+# setup default ansible hosts (Ubuntu only)
+mkdir /etc/ansible 
 cat <<EOF > /etc/ansible/hosts
 [Control-Plane]
 192.168.1.10
@@ -23,8 +25,6 @@ echo "alias ans=ansible"          >> ~/.bashrc
 echo "alias anp=ansible-playbook" >> ~/.bashrc
 
 # auto_pass to ansible_nodes 
-## make a directory 
-mkdir ~/.ssh
 
 ## read hosts from file 
 readarray HOSTS < /etc/hosts
@@ -32,12 +32,12 @@ readarray HOSTS < /etc/hosts
 ### 1.known_hosts##
 if [ ! -f ~/.ssh/known_hosts ]; then
   for HOST in ${HOSTS[@]}; do
-    ssh-keyscan -t ecdsa ${HOSTS} >> ~/.ssh/known_hosts
+    ssh-keyscan -t ecdsa ${HOST} >> ~/.ssh/known_hosts
   done
 fi
 
 ### 2.authorized_keys
-if [ ! -f ~/.ssh/id_rsa.pub ]; then
+if [ ! -f ~/.ssh/authorized_keys ]; then
   ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N ''
     for HOST in ${HOSTS[@]}; do 
        sshpass -p vagrant ssh-copy-id -f ${HOST}
