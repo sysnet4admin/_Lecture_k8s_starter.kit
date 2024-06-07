a6071397983266ea68731fa7d9e84c4b8ab8957a
Hoon Jo <pagaia@hotmail.com>
2024-06-07 13:13:15 +0900

    rearrange dir number

--- ch2/2.3/.cmd
+++ ch2/2.3/.cmd
@@ -1,34 +1,25 @@
-# vmware-fusion (1/3)
+# virtualbox (1/3)
 ## installation 
-### MacOS(arm64)
-### https://formulae.brew.sh/cask/vmware-fusion
-### brew install --cask vmware-fusion
-### https://github.com/Homebrew/homebrew-cask/blob/master/Casks/v/vmware-fusion.rb
-### vmware-fusion v13.5.2
-brew install --cask ./vmware-fusion-v13.5.2/vmware-fusion.rb
+### Windows 
+### https://winget.run/pkg/Oracle/VirtualBox
+### https://winstall.app/apps/Oracle.VirtualBox
+winget install -e --id Oracle.VirtualBox -v 7.0.18
 
-### brew install --cask vagrant-vmware-utility
-### https://github.com/Homebrew/homebrew-cask/blob/master/Casks/v/vagrant-vmware-utility.rb
-### vagrant-vmware-utility v1.0.22
-brew install --cask ./vagrant-vmware-utility-1.0.22/vagrant-vmware-utility.rb
+### MacOS 
+### https://formulae.brew.sh/cask/virtualbox 
+### brew install --cask virtualbox 
+### https://github.com/Homebrew/homebrew-cask/blob/master/Casks/v/virtualbox.rb
+### virtualbox v7.0.18
+brew install --cask ./virtualbox-v7.0.18/virtualbox.rb
 
-## configure vmware-fusion 
-### copy commands for vmware-fusion 
-sudo ./scripts/copy_launchctl-all-vmware-utility.sh 
-sudo ./scripts/copy_netstat-anvp.sh
-
-### run launchctl load vmware-utility.plist
-### (optional) source ~/.zshrc 
-launchctl-load-vmware-utility
-
-### create vnet2 nic for k8s cluster int 
-sudo ./scripts/vf_net_create_vnet2
-
-### check vmnet (optional)
-ls /Library/Preferences/VMware\ Fusion
 
 # vagrant (2/3)
 ## installation 
+### Windows 
+### https://winget.run/pkg/Hashicorp/Vagrant
+### https://winstall.app/apps/Hashicorp.Vagrant
+winget install -e --id Hashicorp.Vagrant -v 2.4.1 
+
 ### MacOS 
 ### https://formulae.brew.sh/cask/vagrant
 ### brew install --cask vagrant
@@ -36,18 +27,15 @@ ls /Library/Preferences/VMware\ Fusion
 ### vagrant v2.4.1 
 brew install --cask ./vagrant-v2.4.1/vagrant.rb
 
-### vagrant-vmware plugin 
-vagrant plugin install vagrant-vmware-desktop
-
-### check vagrant plugin list  
-gem list --remote vagrant- | grep -i vmware
-
-
-# READY TO RUN the `vagrant up` 
-
 
 # tabby (3/3)
 ## installation
+### Windows 
+### https://winget.run/pkg/Eugeny/Tabby
+### https://winstall.app/apps/Eugeny.Tabby
+### winget install -e --id Eugeny.Tabby
+winget install -e --id Eugeny.Tabby -v 1.0.207
+
 ### MacOS 
 ### https://formulae.brew.sh/cask/tabby
 ### brew install --cask tabby
