11cdbffc7154f24df20ac3dc48617580b0580321
Hoon Jo <pagaia@hotmail.com>
2024-08-06 09:51:02 +0900

    upt .cmd

---
+++ B/B.001/.cmd
@@ -0,0 +1,9 @@
+k run net --image=sysnet4admin/net-tools-ifn
+
+k get po -o yaml
+k api-resources
+
+k get po -o yaml | grep -i resource
+k get po -o yaml | grep -C3 -i resource
+k get po -o yaml | grep -i object
+
