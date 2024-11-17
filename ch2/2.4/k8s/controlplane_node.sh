d3ba45103fa68259f13ea407c318faef840e3a34
Hoon Jo <pagaia@hotmail.com>
2024-11-17 13:34:30 +0900

    add kg kd

--- ch2/2.4/k8s/controlplane_node.sh
+++ ch2/2.4/k8s/controlplane_node.sh
@@ -19,7 +19,9 @@ kubectl completion bash >/etc/bash_completion.d/kubectl
 
 # alias kubectl to k 
 echo 'alias k=kubectl'               >> ~/.bashrc
+echo "alias kg='kubectl get'"        >> ~/.bashrc
 echo "alias ka='kubectl apply -f'"   >> ~/.bashrc
+echo "alias kd='kubectl delete -f'"  >> ~/.bashrc
 echo 'complete -F __start_kubectl k' >> ~/.bashrc
 
 # git clone k8s-code
