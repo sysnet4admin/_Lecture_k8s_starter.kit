5ec86fbddcd94de91b808aaed3b161948ecf1c85
Hoon Jo <pagaia@hotmail.com>
2025-03-14 13:04:20 +0900

    upt repo from docker to quay for calico

--- ch2/2.4/k8s/controlplane_node.sh
+++ ch2/2.4/k8s/controlplane_node.sh
@@ -10,9 +10,9 @@ mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
-# CNI raw address & config for kubernetes's network 
-CNI_ADDR="https://raw.githubusercontent.com/sysnet4admin/IaC/master/k8s/CNI"
-kubectl apply -f $CNI_ADDR/172.16_net_calico_v3.26.0.yaml
+# CNI raw address & config for kubernetes's network
+CNI_ADDR="https://raw.githubusercontent.com/sysnet4admin/IaC/main/k8s/CNI"
+kubectl apply -f $CNI_ADDR/calico-quay-v3.29.2.yaml
 
 # kubectl completion on bash-completion dir
 kubectl completion bash >/etc/bash_completion.d/kubectl
